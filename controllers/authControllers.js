8a07430b2440745b16fd7efb226837a8aea73ae3
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-12-11 18:28:25 +0200

    implement login api

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,7 @@
+import bcrypt from "bcrypt";
 import { Customer } from "../models/Customer.js";
 import { asyncErrorHandler } from "../utils/asyncErrorHandler.js";
+import { CustomError } from "../utils/CustomError.js";
 
 export const register = asyncErrorHandler(async (req, res, next) => {
   const { name, email, password, phone } = req.body;
@@ -14,6 +16,27 @@ export const register = asyncErrorHandler(async (req, res, next) => {
   const token = customer.generateJwt();
 
   res.status(201).send({
+    success: true,
+    token,
+  });
+});
+
+export const login = asyncErrorHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const customer = await Customer.findOne({ email });
+  if (!customer) {
+    return next(new CustomError("Invalid email or password", 401));
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, customer.password);
+  if (!isPasswordCorrect) {
+    return next(new CustomError("Invalid email or password", 401));
+  }
+  s;
+  const token = customer.generateJwt();
+
+  res.status(200).send({
     success: true,
     token,
   });
--- router/authRouter.js
+++ router/authRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/authControllers.js";
+import { login, register } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 router.route("/register").post(register);
+router.route("/login").post(login);
 
 export default router;
