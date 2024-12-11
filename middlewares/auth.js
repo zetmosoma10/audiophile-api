2b23ca31ac57d58ffe94d33c728375be736fdea2
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-12-11 16:38:43 +0200

    add auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import CustomError from "../utils/CustomError.js";
+import asyncErrorHandler from "../utils/asyncErrorHandler.js";
+import { Customer } from "../models/Customer.js";
+
+const auth = asyncErrorHandler(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(" ")[1];
+
+  // * check if token exist
+  if (!token) {
+    return next(
+      new CustomError("Authentication token missing. Please login", 401)
+    );
+  }
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_STR);
+
+  const customer = await Customer.findById({ _id: decoded._id });
+  // * check if user for given token exist
+  if (!customer) {
+    return next(
+      new CustomError(
+        "User associated with the token doest not exist. Please login again",
+        401
+      )
+    );
+  }
+
+  req.customer = customer;
+
+  next();
+});
+
+export default auth;
