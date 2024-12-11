a1bde12caa7db65fae8cc66ee481a30f42d4f3d8
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-12-11 11:55:50 +0200

    connect to database

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+import mongoose from "mongoose";
 
-const port = 3000;
+mongoose
+  .connect(process.env.DB_CONN_STR)
+  .then((data) => console.log(`Successfull Connected to database...`))
+  .catch((error) => console.log("FAILD DB connection...", error));
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server listen at port: ${port}`);
