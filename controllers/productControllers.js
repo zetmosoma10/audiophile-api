9664f71b2feb3dd1729517acffcd086d221d70eb
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-12-19 14:58:31 +0200

    implement delete and update product api

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -10,14 +10,14 @@ export const createProduct = asyncErrorHandler(async (req, res) => {
     features: req.body.features,
     category: req.body.category,
     mainImage: {
-      large: req.body.mainImage.large,
-      medium: req.body.mainImage.medium,
-      small: req.body.mainImage.small,
+      large: req.body.mainImage?.large,
+      medium: req.body.mainImage?.medium,
+      small: req.body.mainImage?.small,
     },
     previewImage: {
-      large: req.body.previewImage.large,
-      medium: req.body.previewImage.medium,
-      small: req.body.previewImage.small,
+      large: req.body.previewImage?.large,
+      medium: req.body.previewImage?.medium,
+      small: req.body.previewImage?.small,
     },
     images: req.body.images,
     boxContents: req.body.boxContents,
@@ -59,5 +59,63 @@ export const getProduct = asyncErrorHandler(async (req, res, next) => {
   res.status(200).send({
     success: true,
     product,
+  });
+});
+
+export const deleteProduct = asyncErrorHandler(async (req, res, next) => {
+  const { productId } = req.params;
+
+  const deletedProduct = await Product.findByIdAndDelete(productId);
+  console.log(deletedProduct);
+
+  if (!deletedProduct) {
+    return next(new CustomError("Product already deleted", 400));
+  }
+
+  res.status(200).send({
+    success: true,
+  });
+});
+
+export const updateProduct = asyncErrorHandler(async (req, res, next) => {
+  const { productId } = req.params;
+
+  const productInDb = await Product.findById(productId);
+  console.log(productInDb);
+  if (!productInDb) {
+    return next(new CustomError("Product not found", 404));
+  }
+
+  const { mainImage = {}, previewImage = {} } = req.body;
+
+  const updatedProduct = await Product.findByIdAndUpdate(
+    productId,
+    {
+      $set: {
+        name: req.body.name || productInDb.name,
+        description: req.body.description || productInDb.description,
+        price: req.body.price || productInDb.price,
+        features: req.body.features || productInDb.features,
+        category: req.body.category || productInDb.category,
+        mainImage: {
+          large: mainImage.large || productInDb.mainImage.large,
+          medium: mainImage.medium || productInDb.mainImage.medium,
+          small: mainImage.small || productInDb.mainImage.small,
+        },
+        previewImage: {
+          large: previewImage.large || productInDb.previewImage.large,
+          medium: previewImage.medium || productInDb.previewImage.medium,
+          small: previewImage.small || productInDb.previewImage.small,
+        },
+        images: req.body.images || productInDb.images,
+        boxContents: req.body.boxContents || productInDb.boxContents,
+      },
+    },
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).send({
+    success: true,
+    product: updatedProduct,
   });
 });
--- router/productRouter.js
+++ router/productRouter.js
@@ -2,14 +2,20 @@ import express from "express";
 import auth from "../middlewares/auth.js";
 import {
   createProduct,
+  deleteProduct,
   getAllProduct,
   getProduct,
+  updateProduct,
 } from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.route("/").post(auth, createProduct);
 router.route("/:categoryId").get(auth, getAllProduct);
-router.route("/product-detail/:productId").get(auth, getProduct);
+router
+  .route("/product-detail/:productId")
+  .get(auth, getProduct)
+  .patch(auth, updateProduct)
+  .delete(auth, deleteProduct);
 
 export default router;
