7864df87c16d341de8b3fddfd2a6703438f96b84
Zet <111925382+zetmosoma10@users.noreply.github.com>
2025-01-14 12:21:14 +0200

    add middlewares for security

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
+import helmet from "helmet";
+import xss from "xss-clean";
+import compression from "compression";
+import rateLimit from "express-rate-limit";
+import sanitize from "express-mongo-sanitize";
 import { fileURLToPath } from "url";
 import authRouter from "./router/authRouter.js";
 import categoryRouter from "./router/categoryRouter.js";
@@ -18,7 +24,20 @@ const __dirname = path.dirname(__filename);
 const publicPath = path.join(__dirname, "public");
 // http://localhost:3000/images/users/user1.jpg
 
+rateLimit({
+  windowMs: 15 * 60 * 1000, // * 15 minutes
+  max: 100, // * limit each IP to 100 requests per windowMs // 100 requests
+  message: "Too many requests from this IP, please try again after an hour",
+});
+
+app.use(helmet());
+app.use(cors());
+app.use(sanitize());
+app.use(xss());
+app.use("/api/auth", rateLimit);
+app.use(compression());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(publicPath));
 app.use("/api/auth", authRouter);
 app.use("/api/categories", categoryRouter);
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import mongoose from "mongoose";
+
+process.on("uncaughtException", (error) => {
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(`${error.name}: ${error.message}`);
+
+  process.exit(1);
+});
+
+import app from "./app.js";
 
 mongoose
   .connect(process.env.DB_CONN_STR)
@@ -10,6 +18,15 @@ mongoose
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server listen at port: ${port}`);
+});
+
+process.on("unhandledRejection", (error) => {
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(`${error.name}: ${error.message}`);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
